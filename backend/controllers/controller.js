c8d1dfcf7fdd4664501106efa058f164b166ae17
moramraul <94130850+moramraul@users.noreply.github.com>
2024-09-13 12:59:00 +0200

    Adding magicRequest route and controller function

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,20 +1,35 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 function pruebaRouter(req, res) {
-    console.log('Prueba')
-    res.send({ message: "El router funciona" })
+  console.log("Prueba");
+  res.send({ message: "El router funciona" });
 }
 
 async function sendingMagic(req, res) {
-    const result = await fetch('https://api.scryfall.com/cards/search?order=cmc&q=c%3Ared+pow%3D3')
-    const data = await result.json();
-    console.log(data)
-    res.send(data) 
+  const result = await fetch(
+    "https://api.scryfall.com/cards/search?order=cmc&q=c%3Ared+pow%3D3"
+  );
+  const data = await result.json();
+  console.log(data);
+  res.send(data);
+}
+
+async function magicRequest(req, res) {
+  const data = req.body;
+  console.log(data);
+  res.sendStatus(200);
 }
 
 const control = {
-    pruebaRouter: (req, res) => { pruebaRouter(req, res) },
-    sendingMagic: (req, res) => { sendingMagic(req, res) },
-}
+  pruebaRouter: (req, res) => {
+    pruebaRouter(req, res);
+  },
+  sendingMagic: (req, res) => {
+    sendingMagic(req, res);
+  },
+  magicRequest: (req, res) => {
+    magicRequest(req, res);
+  },
+};
 
 export default control;
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,50 +1,9 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
-import control from '../controllers/controller.js';
+import control from "../controllers/controller.js";
 
-//const controller = require('../controllers/controller');
+router.get("/prueba", control.pruebaRouter);
+router.get("/magic", control.sendingMagic);
+router.post("/magicReq", control.magicRequest);
 
-router.get("/prueba", control.pruebaRouter)
-router.get("/magic", control.sendingMagic)
-// router.post("/register", user.registro)
-// router.post("/login", user.loguear)
-// router.post("/modifyprofile", user.updateUser)
-// //Banear usuario
-// router.post("/banearuser", user.banearUser);
-
-// router.post("/checkout", actionCompras.pago) 
-
-// router.post("/addproduct", admin.addProduct)
-// router.post("/deleteproduct", admin.eraseProduct)
-// router.post("/modifyproduct", admin.modifyProduct)
-
-// //Recoge todas las compras del usuario
-// router.post("/historial", actionCompras.buscarCompras);
-// router.post("/allproductofiltro", products.buscarProductos);
-
-// //Busca cada articulo de devoluciones
-// router.post("/buscararticulo", products.buscarProducto);
-
-// router.post("/actualizarpedido", actionCompras.actualizarPedido);
-// router.post("/quitarpedido", actionCompras.quitarPedido);
-// router.post("/devolucionprimera", actionCompras.devolucionPrimera);
-
-
-// router.post("/busquedalook", products.buscarProductos);
-// // router.get("/resultadolook/:altura/:color/:estilo/:peso/:talla/:target", function (req,res) {
-
-// router.post("/estilosfavoritos", user.addEstilosFav);
-// router.post("/direcciondos", user.addSegundaDireccion)
-
-// router.get("/resultadolook/", (req,res) => {
-//     res.json(req.params)
-
-// })
-// router.post("/cambiarpass", user.cambiarPass);
-
-// router.get("/cambiarpass/:id/:token", user.confirmUserGet);
-
-// router.post("/enviarmail",actionCompras.enviarMail); //prueba test para mail
-
-// router.post("/insertarpasscambiada", user.insertarPassCambiada);
-export default router
+export default router;
